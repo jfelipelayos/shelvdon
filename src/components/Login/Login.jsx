7874cc83114d289decfbaf6dee71518bf045de1c
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-03-18 18:48:40 -0500

    Pending password validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,27 @@
 import './Login.css';
 import GirlWithBooks from '../../assets/images/woman-books.jpg';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+import API from '../../assets/api/users.json';
 
 export const Login = () => {
+
+    const checkEmailCredentials = (value)=>{
+        let found = API.users.find(user => user.email === value);
+
+        if(found === undefined){
+            return true;
+        }
+            return false;        
+    }
+
+    const checkPasswordCredentials = (e, c)=>{
+        let checked = API.users.find(user => user.email === e);
+
+        if(checked !== undefined && checked.password === c){
+            return true;
+        }
+            return false;        
+    }
 
     return (
         <section>
@@ -12,19 +31,6 @@ export const Login = () => {
                         <img src={GirlWithBooks} className="img-fluid rounded login-image" alt="girl-with-books-stack" />
                     </div>
                     <div className="col-md-7 col-sm-12">
-                        {/*  <form className='login-form'>
-                            <div className="form-group email-form">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                            </div>
-                            <div className="form-group password-form">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                            </div>
-                            <button type="submit" className="btn btn-success login-button" style={{minWidth: '150px'}}><a href="/home" style={{color: 'white', textDecoration: 'none'}}>Login</a></button>
-                        </form> */}
-
                         <Formik
                             initialValues={{ email: '', password: '' }}
                             validate={values => {
@@ -36,12 +42,14 @@ export const Login = () => {
                                     errors.email = 'Email is required';                           
                                 } else if  (!values.password) {
                                     errors.password = 'Password is required';
-                                }else if (
+                                } else if (
                                     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                                 ) {
                                     errors.email = 'Invalid email address';
+                                } else if(checkEmailCredentials(values.email)){
+                                    errors.email = 'This user does not exist'
                                 }
-                                /* else if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?\/~_+-=|\]).{8,32}$/i.test(values.password)){
+                                /* else if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/i.test(values.password)){
                                   errors.password = 'Invalid password. Remember it needs at least: one digit [0-9], one lowercase [a-z], one uppercase [A-Z], one special character and at least 8 characters in length.';
                                } */
                                 return errors;
