9bb56397ecc11f9ae19ef2640d2063ade51285a4
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-03-23 21:44:30 -0500

    Redirect to signup and some details have been added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,39 +1,39 @@
 import './Login.css';
 import GirlWithBooks from '../../assets/images/woman-books.jpg';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import  { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import API from '../../assets/api/users.json';
 
 var CryptoJS = require("crypto-js");
 
 export const Login = () => {
     let navigate = useNavigate();
-    let salt = Math.random()*10+'Ant';
+    let salt = Math.random() * 10 + 'Ant';
 
-    const checkEmail = (value)=>{
+    const checkEmail = (value) => {
         let found = API.users.find(user => user.email === value);
 
-        if(found === undefined){
+        if (found === undefined) {
             return true;
         }
-            return false;        
+        return false;
     }
 
-    const checkCredentials = (values)=>{
-       let checked = API.users.find(user => user.email === values.email);
+    const checkCredentials = (values) => {
+        let checked = API.users.find(user => user.email === values.email);
 
-        if(checked !== undefined && checked.password === values.password){
+        if (checked !== undefined && checked.password === values.password) {
             return true;
         }
-            return false;        
+        return false;
     }
 
     return (
-        <section style={{marginTop: '150px'}}>
-            <div className="card mb-3 login-general-card" style={{ backgroundColor: '#2e0000', color: 'white', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}>
+        <section style={{ marginTop: '150px' }}>
+            <div className="card mb-3 login-general-card" style={{ backgroundColor: '#2e0000', color: 'white', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' }}>
                 <div className="row g-0">
-                    <div className="col-md-5">
-                        <img src={GirlWithBooks} className="img-fluid rounded login-image" alt="girl-with-books-stack" />
+                    <div className="col-md-5 image-login-container">
+                        <img src={GirlWithBooks} className="img-fluid rounded login-image" alt="girl-with-books-stack" loading='eager' />
                     </div>
                     <div className="col-md-7 col-sm-12">
                         <Formik
@@ -43,44 +43,50 @@ export const Login = () => {
                                 if (!values.email && !values.password) {
                                     errors.email = 'Email is required';
                                     errors.password = 'Password is required';
-                                } else if  (!values.email) {
-                                    errors.email = 'Email is required';                           
+                                } else if (!values.email) {
+                                    errors.email = 'Email is required';
                                 } else if (
                                     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
                                 ) {
                                     errors.email = 'Invalid email address';
-                                } else if(checkEmail(values.email)){
+                                } else if (checkEmail(values.email)) {
                                     errors.email = 'This user does not exist'
                                 }
 
                                 const passwordRegex = /(?=.?[A-Z])(?=.?[a-z])(?=.*?[0-9])/;
                                 if (!values.password) {
-                                  errors.password = "Password is required";
+                                    errors.password = "Password is required";
                                 } else if (values.password.length < 8) {
-                                  errors.password = "Password must be at leat 8 characters long";
+                                    errors.password = "Password must be at leat 8 characters long";
                                 } else if (!passwordRegex.test(values.password)) {
-                                  errors.password =
-                                    "Password must contain at least: one lowercase, one uppercase and one number";
-                                }else if(!checkCredentials(values)){
+                                    errors.password =
+                                        "Password must contain at least: one lowercase, one uppercase and one number";
+                                } else if (!checkCredentials(values)) {
                                     errors.password = "Password is incorrect. Please check it and try again";
-                                }                            
+                                }
                                 return errors;
                             }}
                             onSubmit={(values, { setSubmitting }) => {
                                 setSubmitting(true);
                                 sessionStorage.setItem('parsley', CryptoJS.HmacSHA1(values.email, salt))
-                                navigate("/home");   
+                                navigate("/home");
                             }}
                         >
                             {({ isSubmitting }) => (
                                 <Form className='login-form'>
-                                    <label htmlFor="email">Email</label> 
-                                    <Field type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                    <ErrorMessage name="email" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
-                                    <label htmlFor="password">Password</label> 
-                                    <Field type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Enter password"/>
-                                    <ErrorMessage name="password" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
-                                    <button type="submit" disabled={isSubmitting} className="btn btn-success login-button" 
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <Field type="email" name="email" className="form-control" id="loginInputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+                                        <ErrorMessage name="email" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <Field type="password" name="password" className="form-control" id="loginInputPassword" placeholder="Enter password" />
+                                        <ErrorMessage name="password" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                    </div>
+                                    <p>If you have not registered yet, enter <a href='/signup' style={{ color: 'yellowgreen' }}>here</a></p>
+                                    <button type="submit" disabled={isSubmitting} className="btn btn-success login-button"
                                     >Log In
                                     </button>
                                 </Form>
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,52 +1,171 @@
 import './Signup.css';
 import PeopleReading from '../../assets/images/readers.png';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useNavigate } from 'react-router-dom';
 
 export const Signup = () => {
 
+    let navigate = useNavigate();
+
+
     return (
-        <section style={{ marginTop: '150px' }}>
+        <article style={{ marginTop: '150px' }}>
             <div className="card mb-3 signup-general-card">
                 <div className="row g-0">
                     <div className="col-md-4">
                         <img src={PeopleReading} className="img-fluid rounded-start" alt="girl-with-books-stack" />
                     </div>
                     <div className="col-md-8">
-                        <form action="">
 
-                            <div className="signup-form">
-                                <div className="section-1">
-                                    <div class="form-group">
-                                        <label for="inputName">Full Name</label>
-                                        <input type="text" class="form-control" id="signup-inputName" aria-describedby="nameHelp" placeholder="Enter full name"/>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                                    </div>
-                                </div>
+                        {/* <Formik initialValues={{ email: '', password: '',  passwordConfirmation:''}}
+                            validate={values => {
+                                const errors = {};
+                                if (!values.email && !values.password && !values.fullName) {
+                                    errors.email = 'Email is required';
+                                    errors.password = 'Password is required';
+                                    errors.fullName = 'Name is required'
+                                } else if (!values.email) {
+                                    errors.email = 'Email is required';
+                                } else if (
+                                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                ) {
+                                    errors.email = 'Invalid email address';
+                                }
 
-                                <div className="section-2">
-                                <div class="form-group">
-                                        <label for="exampleInputPassword1">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password"/>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputPassword1">Confirm password</label>
-                                        <input type="password" class="form-control" id="confirm-password" placeholder="Confirm password"/>
-                                    </div>
-                                </div>
-                            </div>
+                                const passwordRegex = /(?=.?[A-Z])(?=.?[a-z])(?=.*?[0-9])/;
+                                if (!values.password) {
+                                    errors.password = "Password is required";
+                                } else if (values.password.length < 8) {
+                                    errors.password = "Password must be at leat 8 characters long";
+                                } else if (!passwordRegex.test(values.password)) {
+                                    errors.password =
+                                        "Password must contain at least: one lowercase, one uppercase and one number";
+                                }
 
-                            <div className="section-3">
-                            <small id="signup-formHelp" class="form-text text-muted">We'll never share your email with anyone else.</small><br />
-                            <button type="submit" className='btn btn-success' id='signup-button'>Sign Up</button>
-                            </div>
-                            
-                        </form>
+                                if(!values.fullName){
+                                    errors.fullName = "Name is required";
+                                }else if(values.password !== values.passwordConfirmation){
+                                    errors.passwordConfirmation = "Passwords do not match"
+                                }
 
+                                return errors;
+                            }}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setSubmitting(true);
+                                navigate("/login");
+                            }}>
+
+                            {({ isSubmitting }) => (
+                                <Form>
+                                    <div className='signup-form'>
+                                    <div className="section-1">
+                                        <div className="form-group">
+                                            <label htmlFor="inputName">Full Name</label>
+                                            <Field type="text" name="inputName" className="form-control" id="signupInputName" aria-describedby="nameHelp" placeholder="Enter full name" />
+                                            <ErrorMessage name="inputName" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="inputPassword">Password</label>
+                                            <Field type="password" name="inputPassword" className="form-control" id="signupInputPassword" aria-describedby="passwordHelp" placeholder="Type a password" />
+                                            <ErrorMessage name="inputPassword" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+                                    </div>
+
+                                    <div className="section-2">
+                                        <div className="form-group">
+                                            <label htmlFor="inputEmail">Email address</label>
+                                            <Field type="email" name="inputEmail" className="form-control" id="signupInputEmail" aria-describedby="emailHelp" placeholder="Enter email address" />
+                                            <ErrorMessage name="inputEmail" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="inputPasswordConfirmation">Confirm password</label>
+                                            <Field type="password" name="inputPasswordConfirmation" className="form-control" id="signupInputConfirmationPassword" aria-describedby="passwordHelp" placeholder="Confirm your password" />
+                                            <ErrorMessage name="inputPasswordConfirmation" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div className="section-3">
+                                        <small id="signup-formHelp" className="form-text text">We'll never share your email with anyone else.</small><br />
+                                        <button type="submit" disabled={isSubmitting} className='btn btn-success' id='signup-button'>Sign Up</button>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik> */}
+
+                        <Formik
+                            initialValues={{ email: '', password: '' }}
+                            validate={values => {
+                                const errors = {};
+                                if (!values.email && !values.password) {
+                                    errors.email = 'Email is required';
+                                    errors.password = 'Password is required';
+                                } else if (!values.email) {
+                                    errors.email = 'Email is required';
+                                } else if (
+                                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                                ) {
+                                    errors.email = 'Invalid email address';
+                                } 
+
+                                const passwordRegex = /(?=.?[A-Z])(?=.?[a-z])(?=.*?[0-9])/;
+                                if (!values.password) {
+                                    errors.password = "Password is required";
+                                } else if (values.password.length < 8) {
+                                    errors.password = "Password must be at leat 8 characters long";
+                                } else if (!passwordRegex.test(values.password)) {
+                                    errors.password =
+                                        "Password must contain at least: one lowercase, one uppercase and one number";
+                                } 
+                                return errors;
+                            }}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setSubmitting(true);
+                                navigate("/home");
+                            }}
+                        >
+                            {({ isSubmitting }) => (
+                                <Form>
+                                    <div className='signup-form'>
+                                    <div className="section-1">
+                                        {/* <div className="form-group">
+                                            <label htmlFor="inputName">Full Name</label>
+                                            <Field type="text" name="inputName" className="form-control" id="signupInputName" aria-describedby="nameHelp" placeholder="Enter full name" />
+                                            <ErrorMessage name="inputName" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div> */}
+
+                                        <div className="form-group">
+                                            <label htmlFor="inputPassword">Password</label>
+                                            <Field type="password" name="inputPassword" className="form-control" id="signupInputPassword" aria-describedby="passwordHelp" placeholder="Type a password" />
+                                            <ErrorMessage name="inputPassword" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+                                    </div>
+
+                                    <div className="section-2">
+                                        <div className="form-group">
+                                            <label htmlFor="inputEmail">Email address</label>
+                                            <Field type="email" name="inputEmail" className="form-control" id="signupInputEmail" aria-describedby="emailHelp" placeholder="Enter email address" />
+                                            <ErrorMessage name="inputEmail" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="inputPasswordConfirmation">Confirm password</label>
+                                            <Field type="password" name="inputPasswordConfirmation" className="form-control" id="signupInputConfirmationPassword" aria-describedby="passwordHelp" placeholder="Confirm your password" />
+                                            <ErrorMessage name="inputPasswordConfirmation" component="div" style={{ color: 'red', fontWeight: 'bold' }} />
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <div className="section-3">
+                                        <small id="signup-formHelp" className="form-text text">We'll never share your email with anyone else.</small><br />
+                                        <button type="submit" disabled={isSubmitting} className='btn btn-success' id='signup-button'>Sign Up</button>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
                     </div>
                 </div>
             </div>
-        </section>
+        </article>
     )
 }
