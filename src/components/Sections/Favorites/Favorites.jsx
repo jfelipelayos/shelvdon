4c45fed1ac2aed7bd750e55c4e7b9eb05935b395
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-04-06 13:50:56 -0500

    Alphabetical sort menu fully implemented and updated on all sections

--- src/components/Sections/Favorites/Favorites.jsx
+++ src/components/Sections/Favorites/Favorites.jsx
@@ -1,19 +1,50 @@
-import React, {useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import './Favorites.css'
 import { GlobalContext } from '../../../context/GlobalState'
 import AlphabetMenu from '../../AlphabetMenu/AlphabetMenu'
+import { BookFinishedCard } from '../../BookFinishedCard/BookFinishedCard'
 
 
 export const Favorites = () => {
   const { favorites: favorite } = useContext(GlobalContext)
+  let indexedList = {};
+  let [index, setIndex] = useState('');
+
+
+  const setIndexStructure = () => {
+    favorite.map(i => indexedList[i.name[0]] = [])
+  }
+
+  const setIndexValues = () => {
+    setIndexStructure();
+    favorite.map(i => indexedList[i.name[0]].push(i))
+  }
+
+  const sendDataToParent = (index) => {
+    setIndex(index);
+  };
+
+  setIndexValues();
+
   return (
-    <div style={{margin:'0 1.5rem', marginTop:'1rem'}}>Favorite Books
-            <AlphabetMenu/>
-      <ul>
-        {favorite.map((book) => (
-            <li>{book.name}</li>
-          ))}
-      </ul>
+    <div>
+      <div style={{ display: 'flex', flexFlow: 'row', margin: '3% 2% 0.5% 2%' }}>
+        <h2>Favorite books</h2>
+        {index ? (
+          <h2 style={{ marginLeft: '1%' }}>by letter:</h2>
+        ) : null}
+        <h1 style={{ fontWeight: 'bold', color: '#9A4444', marginLeft: '1%', marginTop: '-0.5%' }}>{index}</h1>
+      </div>
+      <AlphabetMenu sendDataToParent={sendDataToParent} />
+      <div className="w-100 h-100 d-flex flex-row justify-content-center align-items-center" style={{ marginTop: '5%' }}>
+        {indexedList[index]?.length > 0 ? (
+          <ul>
+            {indexedList[index]?.map((book) => (
+              <BookFinishedCard book={book} />
+            ))}
+          </ul>
+        ) : (<h2>Empty List</h2>)}
+      </div>
     </div>
   )
 }
