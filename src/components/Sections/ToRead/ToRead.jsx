141ee2caeb89f51ee9adab7bf77c320ad67c853b
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-04-07 13:09:48 -0500

    Sections styles standardized

--- src/components/Sections/ToRead/ToRead.jsx
+++ src/components/Sections/ToRead/ToRead.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react'
-import './ToRead'
 import { GlobalContext } from '../../../context/GlobalState'
 import AlphabetMenu from '../../AlphabetMenu/AlphabetMenu'
 import { BookFinishedCard } from '../../BookFinishedCard/BookFinishedCard'
 import EmptyList from '../../EmptyList/EmptyList'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBroom } from '@fortawesome/free-solid-svg-icons'
+import "../../../styles/Sections.css";
 
 export const ToRead = () => {
   const { toread } = useContext(GlobalContext)
@@ -29,15 +31,16 @@ export const ToRead = () => {
   console.log(toread);
   return (
     <div>
-      <div style={{ display: 'flex', flexFlow: 'row', margin: '3% 2% 0.5% 2%' }}>
+      <div className="subheader-section">
         <h2>Books to read</h2>
         {index ? (
-          <h2 style={{ marginLeft: '1%' }}>by letter:</h2>
+          <h2 className="subheader-title">by letter:</h2>
         ) : null}
-        <h1 style={{ fontWeight: 'bold', color: '#9A4444', marginLeft: '1%', marginTop: '-0.5%' }}>{index}</h1>
+        <h1 className="subheader-title-index">{index}</h1>
+        <button href="#clear" className="subheader-title-button" onClick={(()=>setIndex(''))}><FontAwesomeIcon icon={faBroom}/> <span>Clear filters</span></button>
       </div>      
       <AlphabetMenu sendDataToParent={sendDataToParent} />
-      <div className="w-100 h-100 d-flex flex-row justify-content-center align-items-center" style={{ marginTop: '5%' }}>
+      <div className="w-100 h-100 d-flex flex-row justify-content-center align-items-center subsection-list">
         {indexedList[index]?.length > 0 ? (
           <ul>
             {indexedList[index]?.map((book) => (
