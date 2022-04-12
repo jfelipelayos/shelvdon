46d596ceb87ac2c403b95cba02dda0e10abe943f
Andres Ruiz <johnuribe17@yahoo.com>
2022-04-11 20:56:25 -0500

    in progress 3

--- src/components/Sections/Home/Home.jsx
+++ src/components/Sections/Home/Home.jsx
@@ -20,9 +20,10 @@ export const Home = () => {
     url: `https://hapi-books.p.rapidapi.com/top/2021`,
     headers: {
       'x-rapidapi-host': 'hapi-books.p.rapidapi.com',
-      'x-rapidapi-key': '29832e245cmsh84cf072a4dfae26p1d7ffajsnd0805f8691e0',
+      // 'x-rapidapi-key': '29832e245cmsh84cf072a4dfae26p1d7ffajsnd0805f8691e0',
       // 'x-rapidapi-key': 'cae01b8b10mshb91059cb2e2dee3p1340dcjsn8a0fe768097d',
       // 'x-rapidapi-key': 'eac11bbe89msh89fc5e8eafec9efp1a3f07jsnc1eb77c9c50a',
+      'x-rapidapi-key': '991a7f0984msh3f3099a0170df8bp19fd75jsn273cadcdc26d',
     },
   };
 
