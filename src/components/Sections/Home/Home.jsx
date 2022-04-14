c584b6c8f229dde0143b2fff181e14dff57ce6e1
Paola Andrea <pcadenajoya@hotmail.com>
2022-04-14 12:50:22 -0500

    Paola Update Home Page with Responsive Desing

--- src/components/Sections/Home/Home.jsx
+++ src/components/Sections/Home/Home.jsx
@@ -18,8 +18,7 @@ export const Home = () => {
     url: `https://hapi-books.p.rapidapi.com/top/2021`,
     headers: {
       'x-rapidapi-host': 'hapi-books.p.rapidapi.com',
-      // 'x-rapidapi-key': '29832e245cmsh84cf072a4dfae26p1d7ffajsnd0805f8691e0'
-      'x-rapidapi-key': 'cae01b8b10mshb91059cb2e2dee3p1340dcjsn8a0fe768097d',
+      'x-rapidapi-key': 'f8f6266ad8mshb9452ab8d3f306ap12014fjsne946f0c8d3a8',
     },
   };
 
--- src/components/Sections/Search/Search.jsx
+++ src/components/Sections/Search/Search.jsx
@@ -37,7 +37,7 @@ export const Search = () => {
       url: `https://hapi-books.p.rapidapi.com/search/${bookName}`,
       headers: {
         'x-rapidapi-host': 'hapi-books.p.rapidapi.com',
-        'x-rapidapi-key': '991a7f0984msh3f3099a0170df8bp19fd75jsn273cadcdc26d'
+        'x-rapidapi-key': 'f8f6266ad8mshb9452ab8d3f306ap12014fjsne946f0c8d3a8',
       }
     };
 
